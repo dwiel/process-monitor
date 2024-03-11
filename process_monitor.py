7717bbf0e39a99bb063331bdabf541c85640db3c
Zach Dwiel <zdwiel@gmail.com>
2024-03-10 22:14:26 -0400

    replace standard logger for easier logging when inside app

--- process_monitor.py
+++ process_monitor.py
@@ -1,3 +1,22 @@
+import sys
+
+
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("/tmp/process_monitor.log", "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
+sys.stdout = Logger()
+sys.stderr = sys.stdout
+
 import os
 import time
 import logging
