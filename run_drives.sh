130439ec0a0df2aebbc0a8308f1be19d77be0840
Zach Dwiel <zdwiel@gmail.com>
2025-11-10 22:27:43 -0500

    add default run_drives.sh

--- run_drives.sh
+++ run_drives.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+# cd into the folder which contains your sandbox dir
+cd ~/src
+# mirror your sandbox dir
+drives mirror --live sandbox PASTE_YOUR_HYPERDRIVE_HASH_HERE --filter "/venv/**" --filter "**/__pycache/**" --filter "/.idea/**" --filter "*.pyc" --filter "*.dylib" --filter "/.mypy_cache/**" --filter ".aider*" --filter "/.aider.tags.cache.v3/cache.db" --filter "**/node_modules/**" --verbose
